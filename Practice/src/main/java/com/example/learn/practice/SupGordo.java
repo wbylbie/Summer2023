1e94ae090f3eff177645e85ece94a524c089acae
gordo canchola <gordo.canchola@gsa.gov>
2023-05-04 22:47:18 -0700

    project setup
    Initial changes just to push up a pr
    First off, what's up with all of the capital letters in the package name?
    Changing wrestling to learn since this has nothing to do with wrestling
    
    Adding endpoint mapping to the class "/test"
    Adding endpoint mapping to the single method "/lies"
    
    Updating the package values to reflect the change in directory structure

--- Practice/src/main/java/com/example/learn/practice/SupGordo.java
+++ Practice/src/main/java/com/example/learn/practice/SupGordo.java
@@ -1,12 +1,14 @@
-package com.exmaple.Wrestling.Practice;
+package com.example.learn.practice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class SupGordo {
 
-	@RequestMapping("/")
+  @GetMapping("/lies")
 	public String getMessage() {
 		return "Surprise is the hood!";
 	}
--- Practice/src/test/java/com/example/learn/practice/PracticeApplicationTests.java
+++ Practice/src/test/java/com/example/learn/practice/PracticeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Wrestling.Practice;
+package com.example.learn.practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
