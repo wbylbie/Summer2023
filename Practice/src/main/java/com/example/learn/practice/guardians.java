110ef350bc3acf7807b1ec79ceeff6ca3156b429
wbylbie <wbylbie3@gmail.com>
2023-05-05 20:49:44 -0700

    Added new component for guardians of the galaxy - with endpoint guardians/members

---
+++ Practice/src/main/java/com/example/learn/practice/guardians.java
@@ -0,0 +1,18 @@
+package com.example.learn.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/guardians")
+public class guardians {
+
+	@GetMapping("/members")
+	    public String getMembers() {
+	    return "Star-Lord, Gamora, Drax, Rockett, Groot";
+	    
+	    }
+}
+
+
