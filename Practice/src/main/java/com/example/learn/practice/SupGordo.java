c34f2237c769d942fe7a1c454939a10e8409151f
wbylbie <wbylbie3@gmail.com>
2023-05-05 18:01:46 -0700

    Added two endpoints

--- Practice/src/main/java/com/example/learn/practice/SupGordo.java
+++ Practice/src/main/java/com/example/learn/practice/SupGordo.java
@@ -3,15 +3,30 @@ package com.example.learn.practice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/potato")
 public class SupGordo {
 
   @GetMapping("/lies")
 	public String getMessage() {
 		return "Surprise is the hood!";
 	}
-
+  @GetMapping("/truth")
+  	public String gettingMessage() {
+		return "Alhambra is a G school!";
+	}
+  @GetMapping("/schools/{school}")
+  	public String getSchool(@PathVariable String school) {
+		String str = "";
+	
+		if (school.equals("Willow")) {
+		str = "Sorry Sir, my dad will sue you.";
+		}else if (school.equals("Alhambra")) {
+		str = "What up, homie!";
+		} else str = "hi";
+		return str;
+	}
 
 }
